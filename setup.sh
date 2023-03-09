6927a9c38c2656031a0f8c30b1c8b4cdd3ae27e6
Alicelavander <alicelavander.dev@gmail.com>
2023-03-09 10:13:39 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,15 +6,18 @@ cd ~/.ssh
 cd ~/
 #git clone --recursive git@github.com:Alicelavander/AugmentedNet.git
 wget https://github.com/napulen/AugmentedNet/archive/refs/heads/main.zip
+apt install unzip
 unzip main.zip
 mv AugmentedNet-main AugmentedNet
 #python --version
 #python3 --version
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-12-1_12.1.0-1_amd64.deb
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo dpkg -i cuda-12-1_12.1.0-1_amd64.deb
 sudo apt-get update
-sudo apt-get install cuda-10-0
+sudo apt-get install cuda-12-1
 cd AugmentedNet/
 #python3 -m venv .env 
 #sudo apt install python3.8-venv
@@ -25,4 +28,3 @@ cd AugmentedNet/
 #unzip dataset.zip
 #sudo apt install unzip
 #unzip dataset.zip
-
