e894918d135acc242ece5894667e61aa3daf6b27
Alicelavander <alicelavander.dev@gmail.com>
2023-03-10 12:44:40 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt install gcc
 apt-get install linux-headers-$(uname -r)
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
 dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
 cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
 apt-get update
