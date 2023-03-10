c0df8cf2b6eadd91d0f8621eeea65c6f17537a0f
Alicelavander <alicelavander.dev@gmail.com>
2023-03-10 13:04:34 +0900

    cleanup setup.sh

--- setup.sh
+++ setup.sh
@@ -11,13 +11,11 @@ cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
 apt-get update
 apt-get -y install cuda
 apt install nvidia-cuda-toolkit
-apt-get install zlib1g
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
 add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 apt-get update
 apt-get install libcudnn8=8.8.0.121-1+cuda12.0
 apt-get autoremove
-nvidia-smi
 
 #setup ssh for GitHub
 cd ~/.ssh
@@ -41,3 +39,5 @@ cd AugmentedNet/
 #unzip dataset.zip
 #apt install unzip
 #unzip dataset.zip
+
+nvidia-smi
